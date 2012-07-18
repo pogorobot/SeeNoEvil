a7d230d9af04b20a61539aa5bc2b322f748b7371
Josh Eklund <joshua.s.eklund@gmail.com>
2012-07-17 21:15:37 -0700

    Ceilings and floors now both dynamically generated

--- SeeNoEvil.py
+++ SeeNoEvil.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 
 # Constants
 fps = 30
-width = 540
-height = 720
+width = 660
+height = 480
 stoneColor = (135, 135, 135)
 dragonColor = (42, 42, 95)
 preyColor = (255, 255, 42)
@@ -75,32 +75,58 @@ class Cave(object):
         self.width = width
         self.height = height
         self.ceiling = [] #ceiling is a list of points that we'll use to create an irregular cave
+        self.floor = []   #floor will cover the bottom
         self.backgroundColor = (0, 0, 0)
-        for i in range(self.width / 30):
-            self.ceiling.append((i * 30, random.randint(0, self.height / 4)))
+        self.tileSize = 15 #how many stalactites we draw
+        for i in range((self.width / self.tileSize) + 1):
+            self.ceiling.append((i * self.tileSize, random.randint(0, self.height / 4)))
+        for i in range((self.width / self.tileSize) + 1):
+            self.floor.append((i * self.tileSize, random.randint(self.height * 3 / 4, self.height)))        
     def draw(self):
         screen.fill(self.backgroundColor)
         oldPoint = (0, 0)
-        lower = 0
         for point in self.ceiling:
-            (x, y) = point
-            if oldPoint == (0, 0):
-                oldPoint = point
-                continue
-            (leftX, leftY) = oldPoint
-            if y > leftY:
-                lower = leftY
-                slope = 1
-            else:
-                lower = y
-                slope = 0
-            pygame.draw.rect(screen, wallColor, (leftX, 0, 30, lower))
-            if slope:
-                pygame.draw.polygon(screen, wallColor, [(leftX, leftY), (x, y), (x, leftY)])
-            else:
-                pygame.draw.polygon(screen, wallColor, [(leftX, leftY), (x, y), (leftX, y)])
+            self.drawStalactiteHalf(point, oldPoint)
             oldPoint = point
-        
+        oldPoint = (0, 0)
+        for point in self.floor:
+            self.drawStalagmiteHalf(point, oldPoint)
+            oldPoint = point
+    def drawStalactiteHalf(self, point, oldPoint):
+        (x, y) = point
+        if oldPoint == (0, 0):
+            oldPoint = point
+            return
+        (leftX, leftY) = oldPoint
+        if y > leftY:
+            lower = leftY
+            slope = 1
+        else:
+            lower = y
+            slope = 0
+        pygame.draw.rect(screen, wallColor, (leftX, 0, self.tileSize, lower))
+        if slope:
+            pygame.draw.polygon(screen, wallColor, [(leftX, leftY), (x, y), (x, leftY)])
+        else:
+            pygame.draw.polygon(screen, wallColor, [(leftX, leftY), (x, y), (leftX, y)])
+    def drawStalagmiteHalf(self, point, oldPoint):
+        (x, y) = point                   #set local x and y variables
+        if oldPoint == (0, 0):           #load the leftmost point first
+            return                       #do nothing until we have a range
+        (leftX, leftY) = oldPoint        #set individual variables for the left side coordinates
+        if y < leftY:                    #determine whether slope is positive or negative
+            lower = leftY                #if y-coord on the right is less
+            slope = 0                    #our triangle will point left
+        else:
+            lower = y                    #if y-coord on the right is greater or equal
+            slope = 1                    #our triangle will point right
+        #draw the bottom of the stalagmite - the part underneath both points
+        pygame.draw.rect(screen, wallColor, (leftX, lower, self.tileSize, self.height - lower))
+        #draw the triangle connecting to the next point
+        if slope:                        #the triangle points right
+            pygame.draw.polygon(screen, wallColor, [(leftX, leftY), (x, y), (leftX, y)])
+        else:                            #the triangle points left
+            pygame.draw.polygon(screen, wallColor, [(leftX, leftY), (x, y), (x, leftY)])
 
 def main():
     pygame.init()
