7bbc6edbfe8c362ca9069c37d3d0fb320e727722
Josh Eklund <joshua.s.eklund@gmail.com>
2012-09-01 23:05:47 -0700

    Object-oriented rectangle flying works halfway decently

--- src/Main.hx
+++ src/Main.hx
@@ -4,6 +4,8 @@ import flash.display.Shape;
 import flash.display.StageAlign;
 import flash.display.StageScaleMode;
 import flash.Lib;
+import flash.Vector;
+import flash.Vector;
 
 /**
  * ...
@@ -12,8 +14,8 @@ import flash.Lib;
 
 class Vector
 {
-	var angle : Float;
-	var length : Float;
+	public var angle : Float;
+	public var length : Float;
 	
 	public function new(angle, length)
 	{
@@ -21,13 +23,14 @@ class Vector
 		this.length = length;
 	}
 	
-	function add(vector2 : Vector)
+	public function add(vector2 : Vector)
 	{
 		var x = Math.sin(angle) * length + Math.sin(vector2.angle) * vector2.length;
 		var y = Math.cos(angle) * length + Math.cos(vector2.angle) * vector2.length;
 		length = Math.sqrt(x * x + y * y);
 		angle = 0.5 * Math.PI - Math.atan2(y, x);
 	}
+	
     
 }
 
@@ -42,17 +45,20 @@ class Main
     static var moveY : Float = 0; // the movement per frame of the rectangle on the vertical axis
 	
 	static var gravity : Float = 0.3;
+	
+	static var player : Ghost;
+	
 
     static function main()
     {
-		var player = new Ghost();
+		player = new Ghost();
         myRectangle  = new flash.display.Shape();
         myRectangle.graphics.beginFill ( 0x990000 );
         myRectangle.graphics.lineStyle ( 1, 0x000000, 1, false, flash.display.LineScaleMode.NONE );
         myRectangle.graphics.drawRect ( 0, 0, rectangleWidth, rectangleHeight);
         myRectangle.graphics.endFill ();
 
-        flash.Lib.current.addChild(myRectangle);
+        flash.Lib.current.addChild(player.box);
 
         flash.Lib.current.addEventListener(flash.events.Event.ENTER_FRAME,function(_) Main.onEnterFrame());
        
@@ -63,13 +69,13 @@ class Main
     static function key_down(event:flash.events.KeyboardEvent)
     {
         if (event.keyCode == 37) // left arrow
-            moveX -= 1;
+            player.velocity.add(new Vector(3 * Math.PI / 2, 1));
         else if (event.keyCode == 39) // right arrow
-            moveX += 1;
+            player.velocity.add(new Vector(Math.PI / 2, 1));
         else if (event.keyCode == 38) // up arrow
-            moveY -= 1;
+            player.velocity.add(new Vector(0, 1));
         else if (event.keyCode == 40) // down arrow
-            moveY += 1;
+            player.velocity.add(new Vector(Math.PI, 1));
     }
    
     static function key_up(event:flash.events.KeyboardEvent)
@@ -88,10 +94,8 @@ class Main
 
     static function onEnterFrame()
     {
-        myRectangle.x += moveX;
-        myRectangle.y += moveY;
-		moveY += gravity; //fall
-       
+		player.move();
+		player.draw();
         // here we prevent the rectangle to move out of the display area
         if ( myRectangle.x > flash.Lib.current.stage.stageWidth - rectangleWidth -1)
 		{
@@ -118,12 +122,12 @@ class Main
 
 class Ghost
 {
-	var velocity: Vector;
-	var x: Int;
-	var y: Int;
+	public var velocity: Vector;
+	var x: Float;
+	var y: Float;
 	var width: Int;
 	var height: Int;
-	var box: Shape;
+	public var box: Shape;
 	
 	public function new()
 	{
@@ -133,14 +137,24 @@ class Ghost
 		width = 50;
 		height = 50;
         box  = new flash.display.Shape();
+	}
+	public function draw()
+	{
         box.graphics.beginFill ( 0x990000 );
         box.graphics.lineStyle ( 1, 0x000000, 1, false, flash.display.LineScaleMode.NONE );
         box.graphics.drawRect ( x, y, width, height);
         box.graphics.endFill ();
-		
 	}
-	function draw()
+    public function move()
 	{
-		
+        x += Math.sin(velocity.angle) * velocity.length;
+        y -= Math.cos(velocity.angle) * velocity.length;
+		box.graphics.drawRect(x, y, width, height);
+        /*bounce off walls
+        if (x < width / 2 || x > self.cave.width - self.width / 2)
+            self.angle = -self.angle
+        if self.y < self.height / 2 or self.y > self.cave.height - self.height / 2:
+            self.angle = math.pi - self.angle
+		*/
 	}
 }
